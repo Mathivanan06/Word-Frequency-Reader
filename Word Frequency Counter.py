2c4dc50997a3bc9e48bdf22deedfe22b4ff5ba86
Mathivanan06 <141113274+Mathivanan06@users.noreply.github.com>
2023-08-01 13:34:56 +0530

    Add files via upload

---
+++ Word Frequency Counter.py
@@ -0,0 +1,12 @@
+frequency = {}
+sentence = input("Enter a sentence:")
+
+
+for word in sentence.split():
+    frequency[word] = frequency.get(word, 0)+1
+
+words = frequency.keys()
+words = sorted(words)
+
+for item in words:
+    print(f"{item} = {frequency[item]}")
